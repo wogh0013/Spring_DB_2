fea1635e72495416279e3298f5bf7b80f7b8e061
username <wogh0013@naver.com>
2022-09-24 04:37:25 +0900

    JPA 설정 및 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
